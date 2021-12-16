d21937e144f5b313ca06e708c5fc3df427404c54
spenpal2000 <sanjeev.penupala@gmail.com>
2021-12-16 02:29:36 -0600

    finished part1

---
+++ 2021/day14/day14.py
@@ -0,0 +1,49 @@
+from collections import Counter
+from pprint import pprint
+
+def part1(template, rules):
+    polymer = template
+    for step in range(10):
+        polymer_copy = []
+        for idx in range(len(polymer) - 1):
+            pair = polymer[idx: idx + 2]
+            polymer_copy.append(f'{pair[0]}{rules[pair]}')
+        polymer = ''.join([new_pair for new_pair in polymer_copy]) + polymer[-1]
+
+
+    word_counts = Counter(polymer)
+    most_common_count = word_counts.most_common()[0][1]
+    least_common_count = word_counts.most_common()[-1][1]
+    
+    return most_common_count - least_common_count
+
+def part2(template, rules):
+    polymer = template
+    for step in range(40):
+        polymer_copy = []
+        for idx in range(len(polymer) - 1):
+            pair = polymer[idx: idx + 2]
+            polymer_copy.append(f'{pair[0]}{rules[pair]}')
+        polymer = ''.join([new_pair for new_pair in polymer_copy]) + polymer[-1]
+
+
+    word_counts = Counter(polymer)
+    most_common_count = word_counts.most_common()[0][1]
+    least_common_count = word_counts.most_common()[-1][1]
+    
+    return most_common_count - least_common_count
+
+def main():
+    with open("puzzle_input.txt") as f:
+        template = f.readline().strip()
+        f.readline()
+        rules = {}
+        for line in f:
+            search, insert = line.strip().split(' -> ')
+            rules[search] = insert
+    
+    print(f"Part 1 Answer: {part1(template, rules)}")
+    print(f"Part 2 Answer: {part2(template, rules)}")
+
+if __name__ == "__main__":
+    main()
