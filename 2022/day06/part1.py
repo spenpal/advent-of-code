1fc91eb116ae3d3b262b7744fc75b969e958b48e
spenpal2000 <sanjeev.penupala@gmail.com>
2022-12-05 23:23:12 -0600

    we did day06 :)

---
+++ 2022/day06/part1.py
@@ -0,0 +1,10 @@
+with open("input.txt") as f:
+    data = f.readline()
+    window_length = 4
+    i = 0
+    while i <= len(data)-window_length:
+        four_letters = data[i:i+window_length]
+        if len(set(four_letters)) == window_length: break
+        i += 1
+            
+print(i+window_length)
