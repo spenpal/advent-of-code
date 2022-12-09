4e7359a0cc2bec066ddcc555363fa29e6a9e5bb0
spenpal2000 <sanjeev.penupala@gmail.com>
2022-12-09 01:29:30 -0600

    update touch files to README.md instead of puzzle.md

--- aoc-create.sh
+++ aoc-create.sh
@@ -4,11 +4,11 @@
 mkdir $(date +%Y)
 
 # Then inside the year directory, create 25 directories from day01 to day25. 
-# Inside each of these directories, create 4 files named input.txt, part1.py, part2.py, and puzzle.md
+# Inside each of these directories, create 4 files named input.txt, part1.py, part2.py, and README.md
 for i in {01..25}; do
     mkdir $(date +%Y)/day$i;
     touch $(date +%Y)/day$i/input.txt;
     touch $(date +%Y)/day$i/part1.py;
     touch $(date +%Y)/day$i/part2.py;
-    touch $(date +%Y)/day$i/puzzle.md;
+    touch $(date +%Y)/day$i/README.md;
 done
