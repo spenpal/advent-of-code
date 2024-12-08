b19ae91d8af644545af8dedea7e61dd1abae535a
spenpal <spenpal2000@gmail.com>
2024-12-07 23:17:21 -0600

    day6 done!!!!!!

---
+++ src/2024/d06.py
@@ -0,0 +1,85 @@
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+Path = list[tuple[tuple[int, int], tuple[int, int]]]
+
+
+def parse(data: str) -> dict[int, tuple]:
+    data = [list(line.strip()) for line in data.strip().splitlines()]
+    return {1: (data,), 2: (data,)}
+
+
+def find_path(
+    grid: list[list[str]], pos: tuple[int, int], direction: tuple[int, int]
+) -> Path | None:
+    def valid_pos(x: int, y: int) -> bool:
+        return 0 <= x < len(grid) and 0 <= y < len(grid[0])
+
+    def rotate(direction: tuple[int, int]) -> tuple[int, int]:
+        return DIRECTIONS[(DIRECTIONS.index(direction) + 1) % len(DIRECTIONS)]
+
+    path, visited = [], set()
+    x, y = pos
+    dx, dy = direction
+    while True:
+        step = (x, y), (dx, dy)
+        if step in visited:
+            return None
+
+        path.append(step)
+        visited.add(step)
+
+        next_x, next_y = (x + dx), (y + dy)
+        if not valid_pos(next_x, next_y):
+            break
+        if grid[next_x][next_y] == "#":
+            dx, dy = rotate((dx, dy))
+        else:
+            x, y = next_x, next_y
+
+    return path
+
+
+def get_start_pos(grid: list[list[str]]) -> tuple[int, int] | None:
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "^":
+                return (i, j)
+    return None
+
+
+def part1(grid: list[list[str]]) -> int:
+    pos = get_start_pos(grid)
+    if not pos:
+        return -1
+
+    path = find_path(grid, pos, DIRECTIONS[0])
+    if not path:
+        return -1
+
+    return len({pos for pos, _ in path})
+
+
+def part2(grid: list[list[str]]) -> int:
+    pos = get_start_pos(grid)
+    if not pos:
+        return -1
+
+    path = find_path(grid, pos, DIRECTIONS[0])
+    if not path:
+        return -1
+
+    visited = set()
+    total = 0
+    for i in range(1, len(path)):
+        obstacle_pos = path[i][0]
+        pos, direction = path[i - 1]
+        if obstacle_pos in visited or obstacle_pos == pos:
+            continue
+
+        grid[obstacle_pos[0]][obstacle_pos[1]] = "#"
+        total += not find_path(grid, pos, direction)
+        grid[obstacle_pos[0]][obstacle_pos[1]] = "."
+
+        visited.add(obstacle_pos)
+
+    return total
